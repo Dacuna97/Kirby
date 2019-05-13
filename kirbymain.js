fa97e67bd87b14b1af10b8c0bed52dc69d3ec262
Noeliac <ncalde01@ucm.es>
2019-05-13 10:17:56 +0200

    minor changes

--- kirbymain.js
+++ kirbymain.js
@@ -992,6 +992,15 @@ var game = function () {
         });
 
         Q.scene("hudsElements", function (stage) {
+            let array =[];
+            array = Q.state.get("score").toString().split("");
+            array.forEach((elem,index) => {
+                stage.insert(new Q.NumberE({
+                    x: 50 + index * 8,
+                    y: 206,
+                    n: elem
+                }));
+            });
 
             stage.insert(new Q.KirbyE({
                 x: 39,
@@ -1023,6 +1032,8 @@ var game = function () {
                 x: 159,
                 y: 198
             }));
+
+
         });
 
         Q.scene("level1", function (stage) {
@@ -1061,7 +1072,7 @@ var game = function () {
         Q.loadTMX("kirbyBG.tmx, kirbyBG2.tmx", function () {
             Q.state.reset({
                 level: 1,
-                score: 0,
+                score: 1234567,
                 health: 6,
                 powers: "normal",
                 lives: 4
